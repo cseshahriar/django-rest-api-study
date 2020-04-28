3c0b11b3ef8dd6497e3d79a486b038b42c594c63
cseshahriar <cse.shahriar.hosen@gmail.com>
2020-04-28 12:58:20 +0600

    Serializer

--- api_basic/models.py
+++ api_basic/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 class Article(models.Model):
     title = models.CharField(max_length=100)
+    # body  = models.TextField()
     author = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    date = models.DateTimeField(auto_now_add=True)
+    email  = models.CharField(max_length=100)
+    date    = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
